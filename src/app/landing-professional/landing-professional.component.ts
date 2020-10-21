63408b9843a538c0f4dd18f51637d83e549c8ac1
glombardo <gianf.lombardo@gmail.com>
2020-10-21 18:28:44 +0200

    Form in professional landing. NO FINISHED

--- src/app/landing-professional/landing-professional.component.ts
+++ src/app/landing-professional/landing-professional.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as AOS from 'aos';
+import { FirebaseService } from '../firebase.service';
 
 @Component({
   selector: 'app-landing-professional',
@@ -8,10 +10,53 @@ import * as AOS from 'aos';
 })
 export class LandingProfessionalComponent implements OnInit {
 
-  constructor() { }
+  form_submitted = false;
+  form_sending = false;
+  form_notify: FormGroup;
+  form_result = null;
+  form_cleaned = false;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private firebase: FirebaseService) {
+  }
 
   ngOnInit(): void {
     AOS.init();
+    this.buildForm();
+  }
+
+  private buildForm(): any {
+    this.form_notify = this.formBuilder.group({
+      name: [''],
+      email: ['', [Validators.required, Validators.email]],
+      type: [1, Validators.required]
+    });
+
+  }
+
+  notify(event: Event): any {
+    event.preventDefault();
+    this.form_submitted = this.form_sending = true;
+    if (this.form_notify.valid) {
+      this.firebase.newSuscriber(this.form_notify.value).then(res => {
+        this.form_result = true;
+        this.form_submitted = false;
+        this.form_cleaned = false;
+        this.form_notify.reset();
+      }).catch(err => {
+        this.form_result = false;
+        console.log(err);
+      }).finally(() => {
+        this.form_sending = false;
+        this.form_cleaned = false;
+      });
+    } else {
+      this.form_sending = false;
+      this.form_cleaned = true;
+    }
+
   }
 
 }
